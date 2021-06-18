01b993e6dd0f8de5784d5a16607497c79b180e86
Anomalous Alpha <blackcarrera@msn.com>
2021-06-18 17:57:38 -0600

    working on more styling of comment post boxes. changed the handlebars input to textarea as wel as in the js file that handles posts, input text now wrapps, and can still post a blog

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,8 +1,8 @@
 async function newFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
-  const message = document.querySelector('input[name="post-message"]').value;
+  const title = document.querySelector('textarea[name="post-title"]').value;
+  const message = document.querySelector('textarea[name="post-message"]').value;
 
   const response = await fetch(`/api/posts`, {
     method: 'POST',
