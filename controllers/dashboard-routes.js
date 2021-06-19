628cf6632ade6b002ee65246ce2a2e8c9eced498
Anomalous Alpha <blackcarrera@msn.com>
2021-06-19 11:44:57 -0600

    had to dial back some code changes as we broke the like feature, now restored plus have newest to oldens posts sh, but we want most recent posts at the top

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -18,6 +18,9 @@ router.get('/', withAuth, (req, res) => {
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
+    order: [[
+      'created_at', 'DESC'
+    ]],
     include: [
       {
         model: Comment,
